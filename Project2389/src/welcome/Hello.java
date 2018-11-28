1afd4bdce7f5ae120a32fb1aab374e3bd20d7c69
Megha Mapalagama <Megha Mapalagama@DESKTOP-3J2JDOP>
2018-11-28 06:55:03 -0500

    First Project

---
+++ Project2389/src/welcome/Hello.java
@@ -0,0 +1,11 @@
+package welcome;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone and Welcome to Class");
+		System.out.println("Hello Everyone and Welcome to Seleniium");
+
+	}
+
+}
